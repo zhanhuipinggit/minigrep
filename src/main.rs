daaf32be3bee0449a6446496299091ccd549aea4
PHP-湛慧平 <zhanhuiping@taqu.cn>
2022-10-28 18:47:47 +0800

    错误标准输入输出 命令行完结

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+use std::env;
+use std::process;
+
+extern crate minigrep;
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("problem parsing arguments:{}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) =  minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+
+
+
 }
