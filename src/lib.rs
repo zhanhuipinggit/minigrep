c364194a9a0ca99d454f2a1e23e72137e78f8ba1
PHP-湛慧平 <zhanhuiping@taqu.cn>
2022-10-31 18:04:23 +0800

    迭代器修复一些问题

--- src/lib.rs
+++ src/lib.rs
@@ -45,13 +45,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let  mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 
